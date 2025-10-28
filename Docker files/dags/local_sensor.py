9dc59ddf9c096f34433b5d1570a231d3dd8fb6b7
Jose Aguilar <jaguilar@foursquare.com>
2025-10-28 16:12:45 +0100

    Created ingestion DAG, adding Docker container to it

--- Docker files/dags/local_sensor.py
+++ Docker files/dags/local_sensor.py
@@ -1,25 +1,30 @@
 from airflow.sdk import dag, task
-from airflow.sensors.filesystem import FileSensor
-from airflow.operators.bash_operator import BashOperator
+from airflow.providers.standard.sensors.filesystem import FileSensor
+from airflow.operators.bash import BashOperator
 from pendulum import datetime
 
-with DAG(
+@dag(
     schedule="10 0 * * *",
     start_date=datetime(2025,10,28),
     description="DAG to ingest events of the previous day",
     tags=["events_ingestion", "daily", "output_snowflake"],
     max_consecutive_failed_dag_runs=3
-)as dag:
+)
+
+def local_sensor():
     
-    #@task
     file_sensor = FileSensor(
         task_id="waiting_for_events_file",
-        filepath="/Users/jaguilar/Desktop/Portfolio/test_folder/dropoff_folder/*.csv",
+        filepath="/dropoff_folder/events_table.csv",
         poke_interval = 60*5,
         timeout = 60*15
     )
 
     file_ingestion = BashOperator(
         task_id="processing_file",
-        bash_command="python3 ../ingestion_pipeline.py"
+        bash_command="python3 scripts/ingestion_pipeline.py"
     )
+
+    file_sensor >> file_ingestion
+
+dag = local_sensor()
